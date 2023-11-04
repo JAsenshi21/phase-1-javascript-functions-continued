04979e889e26388a84fc6950f4c56305caa82f6e
jeremy <jeremyaleman468@gmail.com>
2023-11-04 17:31:44 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(special = "*"){
+     return function innerWrap  (message = "special") {
+        let emphatic = `You are ${special}${message}${special}!`
+        return emphatic
+    }
+    
+}
